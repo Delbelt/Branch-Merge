eb17a21086d878dd9c4433a98d09443e2a8073fa
Nahuel Flores <nahuelhernanflores25@gmail.com>
2022-03-22 06:44:42 -0300

    change functions file

--- functions.js
+++ functions.js
@@ -1,4 +1,10 @@
-const funcionNahuel = () =>
+const funcionString = () =>
 {
     return "Creada por la rama: Nahuel"
+}
+
+const funcionNumber = (max, min) =>
+{
+    //Retorna un numero entre 2 rangos (incluidos los limites)
+    return Math.floor((Math.random() * ((max + 1)-min)) + min)
 }
